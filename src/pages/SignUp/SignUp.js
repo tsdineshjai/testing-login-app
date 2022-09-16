b676511e8e33e01cc179d5d73b60ccf0f988f4c9
tanishka <tsdineshjai@gmail.com>
2022-09-16 23:13:53 +0530

    testing for signupform done

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,12 +1,15 @@
-
-
 const SignUp = () => {
-
-  return(
-    <div className="header">
-      <h3>Sign Up</h3>
-    </div>
-  );
-}
+	return (
+		<div className="header">
+			<h3>Sign Up</h3>
+			<label htmlFor="username">User name</label>
+			<input type="text" id="username" />
+			<label htmlFor="email">email</label>
+			<input type="text" id="email" />
+			<label htmlFor="password">password</label>
+			<input type="password" id="password" />
+		</div>
+	);
+};
 
 export default SignUp;
--- src/pages/SignUp/__test__/SignUp.spec.js
+++ src/pages/SignUp/__test__/SignUp.spec.js
@@ -2,9 +2,36 @@ import { render, screen } from "@testing-library/react";
 import SignUp from "../SignUp";
 
 describe("SignUp", () => {
-	it("has a header", () => {
-		render(<SignUp />);
-		const headerElement = screen.getByRole("heading", { name: /sign up/i });
-		expect(headerElement).toBeInTheDocument();
+	describe("layout", () => {
+		it("has a header", () => {
+			render(<SignUp />);
+			const headerElement = screen.getByRole("heading", { name: /sign up/i });
+			expect(headerElement).toBeInTheDocument();
+		});
+
+		it("has a input for username", () => {
+			render(<SignUp />);
+			expect(screen.getByLabelText("User name")).toBeInTheDocument();
+		});
+		it("has a input for email", () => {
+			render(<SignUp />);
+			expect(screen.getByLabelText("email")).toBeInTheDocument();
+		});
+		it("has a input for password", () => {
+			render(<SignUp />);
+			expect(screen.getByLabelText("password")).toBeInTheDocument();
+		});
+
+		it("checking for a type attribute in password input", () => {
+			render(<SignUp />);
+			expect(screen.getByLabelText("password").type).toBe("password");
+		});
+
+    it("checking for a password repeat", () => {
+      render(<SignUp />);
+      expect(screen.getByLabelText("repeat password")).toBeInTheDocument();
+    })
+
+    
 	});
 });
